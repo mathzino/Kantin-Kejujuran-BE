db817891e2dcb84d22c590092c93f8e945aa691c
mathzino <fairusramadhani29@gmail.com>
2022-07-04 05:51:37 +0700

    .

--- app/auth/router.js
+++ app/auth/router.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 let { signup, signin } = require("./controller");
-
+router.get("/", (req, res) => {
+  res.send("halo kamu");
+});
 router.post("/signup", signup);
 router.post("/signin", signin);
 
