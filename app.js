21e7419bea8f8fe202e18d00b5c2b914b6b5b510
mathzino <fairusramadhani29@gmail.com>
2022-07-05 00:26:10 +0700

    .

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use("public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 // app.use("/adminlte", express.static(path.join(__dirname, "./node_modules/admin-lte/")));
 app.use(
   session({
