61342d0878a6888935185e3a3cc731a7e34ae11c
mathzino <fairusramadhani29@gmail.com>
2022-07-06 11:16:39 +0700

    .

--- app/balance/controller.js
+++ app/balance/controller.js
@@ -12,17 +12,17 @@ module.exports = {
     }
   },
 
-  actionEdit: async (req, res) => {
+  actionEdit: async (req, res, next) => {
     try {
       console.log(req.body);
-      const { balance } = req.body;
-
+      let { balance } = req.body;
       await Balance.findByIdAndUpdate("62c21f5f7767e8ad4b04aad0", {
         balance,
       });
-      this.getBalance();
+
       req.flash("alertMessage", "Berhasil Ubah student");
       req.flash("alertStatus", "success");
+      balance = await Balance.findById("62c21f5f7767e8ad4b04aad0");
       res.status(200).json({ balance });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
